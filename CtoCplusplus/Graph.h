a187be17c2791a0d0fa53cfb53b722d32f80e487
Draft00 <50637382+Draft00@users.noreply.github.com>
2020-11-06 11:58:29 +0300

    Old idea snapshot

--- CtoCplusplus/Graph.h
+++ CtoCplusplus/Graph.h
@@ -1,14 +1,22 @@
 #ifndef GRAPH_H
 #define GRAPT_H
 
+#include <vector>
+
 #define MAX_PATH 260
-#define MAX_SYMBOLS 200
+
+class IPrintInfo
+{
+public:
+	virtual void print_answer(const char file_name[MAX_PATH]) const;
+};
 
 class Graph
 {
 public:
 	Graph();
 	void get_adjacency(const char file_name[MAX_PATH]);
+	void print_adj_matrix(void) const;
 
 	~Graph();
 
@@ -17,17 +25,22 @@ protected:
 	int** adjacency_matrix = nullptr;
 	bool oriented = 0;
 
-	void print_adj_matrix(void) const;
 	
 private:
 	void malloc_matrix();
 };
 
-class Graph_Routing : public Graph//for L3. infinity is -1
+class Graph_Routing : public Graph, public IPrintInfo //for L3. infinity is -1
 {
+public:
 	Graph_Routing();
-
+	void Dijkstra(int start, int finish);
+	void Net_planning(int time);
 	~Graph_Routing() {};
+private:
+	vector<vector<int>> adjacency_list;
+
+	void adj_to_list(void);
 };
 
 #endif //!GRAPH_H
